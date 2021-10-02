168ff971188eacadba25fb616b2f418b599451cb
Jaana Mäll <jaana.mall@hotmail.com>
2021-10-02 18:29:11 +0300

    init commit

---
+++ pages/cats/_name.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Catch all</h1>
+</template>
+
+<script>
+export default {
+  validate({params}) {
+   return /^[a-zA-Z]+$/.test(params.name)
+  }
+}
+</script>
+
+<style>
+
+</style>
